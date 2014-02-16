4d00aeeb6483481e79e4d2ce419597907dbe9e5e
hecomi <info@hecomi.com>
2014-02-16 12:33:34 +0900

    add macro interface

--- routes/macro/index.js
+++ routes/macro/index.js
@@ -1,5 +1,12 @@
 var request = require('request');
+var printf  = require('printf');
 var _       = require('underscore');
+
+var InvalidMacroError = function(msg) {
+	var e = new Error(msg);
+	e.name = 'InvalidMacroError';
+	return e;
+};
 
 var redirect = function(res, url) {
 	return function(req, res) {
@@ -8,17 +15,23 @@ var redirect = function(res, url) {
 };
 
 module.exports = function(app) {
-	var routes = {
-		'mac/monitor/off': function(req, res) {
-			var url = app.get('address') + '/device/wemo/monitor/off';
-			request.get(url).pipe(res);
-		},
-		'mac/monitor/on': function(req, res) {
-			var url = app.get('address') + '/device/wemo/monitor/on';
-			request.get(url).pipe(res);
-		},
-	};
 	return function(req, res) {
-		if (req.params[0]);
+		var routes = {
+			'projector/shutdown': function() {
+				_(2).times(function(n) {
+					setTimeout(function() {
+						var req = request.get(app.get('address') + '/projector/off');
+						if (n == 1) req.pipe(res);
+					}, 1000 * n);
+				});
+			}
+		};
+		for (var macro in routes) {
+			if (req.params[0] === macro) {
+				routes[macro](req.params[0]);
+				return;
+			}
+		}
+		throw InvalidMacroError(printf('"%s" is not defined as Macro API', req.params[0]));
 	};
 };
