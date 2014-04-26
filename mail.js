3c4bd67cd74f226f6c12614bbb967531a6427e28
hecomi <info@hecomi.com>
2014-04-26 22:19:02 +0900

    add mail trigger

---
+++ mail.js
@@ -0,0 +1,24 @@
+var inbox     = require('inbox');
+var _         = require('underscore');
+var settings  = require('./settings');
+var utilities = require('./utilities');
+
+var client = inbox.createConnection(false, 'imap.gmail.com', {
+	secureConnection: true,
+	auth: settings.gmail
+});
+
+client.on('connect', function() {
+	console.log('connected');
+	client.openMailbox('INBOX', function(error){
+		if (error) throw error;
+	});
+});
+
+client.on("new", function(msg) {
+	if (_.findWhere(msg.to, { address: settings.gmail.trigger_address })) {
+		utilities.query(msg.title);
+	}
+});
+
+client.connect();
