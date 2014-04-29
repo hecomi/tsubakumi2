6d392e7662759511eaa95007248c2b35744c3b37
hecomi <info@hecomi.com>
2014-04-29 15:34:10 +0900

    update controller

--- views/public/js/main.js
+++ views/public/js/main.js
@@ -1,4 +1,25 @@
 $(function() {
+	var DEVICE_API_URL  = 'http://' + location.hostname + ':23456';
+	var CONTROL_API_URL = 'http://' + location.host;
+
+	var Netatmo = {
+		roomTag:
+			'<p class="netatmo-content">' +
+				'<span class="title">[室内]</span>' +
+				'<span class="key">気温</span><span class="value"><%= temperature %>℃</span>' +
+				'<span class="key">湿度</span><span class="value"><%= humidity %>%</span>' +
+				'<span class="key">気圧</span><span class="value"><%= pressure %> mbar</span>' +
+				'<span class="key">騒音</span><span class="value"><%= noise %> dB</span>' +
+				'<span class="key">CO<sub>2</sub> </span><span class="value"><%= co2 %> ppm</span>' +
+			'</p>',
+		outsideTag:
+			'<p class="netatmo-content">' +
+				'<span class="title">[室外]</span>' +
+				'<span class="key">気温</span><span class="value"><%= temperature %>℃</span>' +
+				'<span class="key">湿度</span><span class="value"><%= humidity %>%</span>' +
+			'</p>'
+	};
+
 	var Notification = {
 		$target: $('#notification'),
 		duration: 3000,
@@ -24,7 +45,7 @@ $(function() {
 		}
 	};
 
-	$.getJSON('/apis', function(json) {
+	$.getJSON(CONTROL_API_URL + '/apis', function(json) {
 		json.apis.forEach(function(api) {
 			$('<p class="api">' + api + '</p>')
 				.appendTo('#apis')
@@ -41,5 +62,13 @@ $(function() {
 		});
 	}).fail(function() {
 		Notification.error('Something wrong occurs in API server. Please reload.');
+	});
+
+	$.getJSON(DEVICE_API_URL + '/device/netatmo/room?callback=?', function(json) {
+		$('#room').html( ejs.render(Netatmo.roomTag, json) );
+	});
+
+	$.getJSON(DEVICE_API_URL + '/device/netatmo/outside?callback=?', function(json) {
+		$('#outside').html( ejs.render(Netatmo.outsideTag, json) );
 	});
 });
