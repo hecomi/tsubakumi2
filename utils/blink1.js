5c07414d6cd8dc686b7aaa599f4a9da61111be7c
hecomi <info@hecomi.com>
2015-09-28 02:25:57 +0900

    error handling for blink1

--- utils/blink1.js
+++ utils/blink1.js
@@ -1,18 +1,28 @@
-var settings      = require('../settings').blink1;
-var Blink1        = require('node-blink1');
-var blink1        = new Blink1(settings.serial);
+var settings = require('../settings').blink1;
+var Blink1 = require('node-blink1');
+var isFound = true;
+try {
+	var blink1 = new Blink1(settings.serial);
+} catch (e) {
+	console.error(e.stack);
+	isFound = false;
+}
 
 module.exports = {
 	color: (r, g, b) => {
-		setColor(first, second);
+		if (isFound) setColor(first, second);
 	},
 	red: () => {
-		blink1.setRGB(200, 0, 0);
+		if (isFound) blink1.setRGB(200, 0, 0);
 	},
 	green: () => {
-		blink1.setRGB(0, 200, 0);
+		if (isFound) blink1.setRGB(0, 200, 0);
 	},
 	blue: () => {
-		blink1.setRGB(0, 0, 200);
+		if (isFound) blink1.setRGB(0, 0, 200);
 	}
 };
+
+process.on('uncaughtException', err => {
+	console.error(err.stack);
+});
