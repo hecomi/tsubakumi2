4204d77b3185a545e14f3ff3c220d86e1882b906
hecomi <info@hecomi.com>
2015-09-28 02:31:42 +0900

    weaken light intensity during the night

--- utils/blink1.js
+++ utils/blink1.js
@@ -1,6 +1,7 @@
 var settings = require('../settings').blink1;
 var Blink1 = require('node-blink1');
 var isFound = true;
+
 try {
 	var blink1 = new Blink1(settings.serial);
 } catch (e) {
@@ -8,20 +9,31 @@ try {
 	isFound = false;
 }
 
-module.exports = {
+var hour = (new Date()).getHours();
+var isDayTime = hour > 2 && hour < 8;
+
+var utils = {
 	color: (r, g, b) => {
-		if (isFound) setColor(first, second);
+		if (isFound) {
+			if (isDayTime) {
+				blink1.setRGB(r, g, b);
+			} else {
+				blink1.setRGB(r * 0.25, g * 0.25, b * 0.25);
+			}
+		}
 	},
 	red: () => {
-		if (isFound) blink1.setRGB(200, 0, 0);
+		if (isFound) this.color(200, 0, 0);
 	},
 	green: () => {
-		if (isFound) blink1.setRGB(0, 200, 0);
+		if (isFound) this.color(0, 200, 0);
 	},
 	blue: () => {
-		if (isFound) blink1.setRGB(0, 0, 200);
+		if (isFound) this.color(0, 0, 200);
 	}
 };
+
+module.exports = utils;
 
 process.on('uncaughtException', err => {
 	console.error(err.stack);
